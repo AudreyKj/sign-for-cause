50f36ef9b2fd2e7a7e473fa6ee90487af88979f2
AUDREY KADJAR <audreykadjar@gmail.com>
2020-05-17 22:13:49 +0200

    save fix

--- public/script.js
+++ public/script.js
@@ -1,10 +1,7 @@
 let canvas = document.querySelector("canvas");
 let colorInput = document.getElementById("signature-color");
 
-console.log("colorInput", colorInput);
-
 colorInput.addEventListener("input", function() {
-    console.log("colorInput", colorInput.value);
     context.strokeStyle = colorInput.value;
 });
 
@@ -20,24 +17,17 @@ context.lineWidth = 0.5;
 canvas.addEventListener("mousedown", function(e) {
     x = e.offsetX;
     y = e.offsetY;
-    console.log("x", x);
-    console.log("y", y);
-    console.log("e.offsetX", e.offsetX);
-    console.log("e.offsetY", e.offsetY);
+
     drawing = true;
     context.beginPath();
     context.moveTo(x, y);
 });
 
 canvas.addEventListener("mousemove", function(e) {
-    console.log("x2", x2);
-    console.log("y2", y2);
     x2 = e.offsetX;
     y2 = e.offsetY;
 
     if (drawing === true) {
-        console.log("canvas offsetLeft", canvas.offsetLeft);
-        console.log("canvas offsetTop", canvas.offsetTop);
         drawline(x2, y2);
     }
 });
@@ -47,75 +37,40 @@ document.addEventListener("mouseup", function() {
 
     let dataURL = canvas.toDataURL("image/png", 1.0);
     document.getElementById("signature").value = dataURL;
-    //$("input#sign").val($("canvas")[0].toDataurl());
-    console.log("data URL", dataURL);
+
     context.closePath();
 });
 
 //responsive
 canvas.addEventListener("touchstart", function(e) {
+    e.preventDefault();
     x = e.offsetX;
     y = e.offsetY;
-    console.log("x", x);
-    console.log("y", y);
-    console.log("e.offsetX", e.offsetX);
-    console.log("e.offsetY", e.offsetY);
+
     drawing = true;
     context.beginPath();
     context.moveTo(x, y);
 });
 
 canvas.addEventListener("touchmove", function(e) {
-    console.log("x2", x2);
-    console.log("y2", y2);
+    e.preventDefault();
     x2 = e.offsetX;
     y2 = e.offsetY;
 
     if (drawing === true) {
-        console.log("canvas offsetLeft", canvas.offsetLeft);
-        console.log("canvas offsetTop", canvas.offsetTop);
         drawline(x2, y2);
     }
 });
 
 document.addEventListener("touchend", function() {
+    e.preventDefault();
     drawing = false;
 
     let dataURL = canvas.toDataURL("image/png", 1.0);
     document.getElementById("signature").value = dataURL;
-    //$("input#sign").val($("canvas")[0].toDataurl());
-    console.log("data URL", dataURL);
+
     context.closePath();
 });
-
-//prevent scroll on touch
-document.body.addEventListener(
-    "touchstart",
-    function(e) {
-        if (e.target == canvas) {
-            e.preventDefault();
-        }
-    },
-    false
-);
-document.body.addEventListener(
-    "touchend",
-    function(e) {
-        if (e.target == canvas) {
-            e.preventDefault();
-        }
-    },
-    false
-);
-document.body.addEventListener(
-    "touchmove",
-    function(e) {
-        if (e.target == canvas) {
-            e.preventDefault();
-        }
-    },
-    false
-);
 
 function drawline(x2, y2) {
     context.lineTo(x2, y2);
