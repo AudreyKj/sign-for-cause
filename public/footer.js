875486b796ce9c9da0435c6470f961d37b9682f7
AUDREY KADJAR <audreykadjar@gmail.com>
2020-05-22 13:53:04 +0200

    save edits

---
+++ public/footer.js
@@ -0,0 +1,10 @@
+const close = document.querySelector("span.footer-close");
+const footer = document.querySelector("footer");
+
+close.addEventListener("click", function() {
+    footer.style.display = "none";
+});
+
+footer.addEventListener("click", function() {
+    footer.style.display = "none";
+});
